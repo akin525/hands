a43c1a1d60af26c0d3e6c0a71f5b3c831d52e296
IT <akinlabisamson15@gmail.com>
2022-09-10 14:15:15 +0100

    10/09/2022

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,7 @@
             <div class="col-md-6 col-lg-5">
                 <div class="login-box bg-white box-shadow border-radius-10">
                     <div class="login-title">
-                        <h2 class="text-center text-primary">Login To DeskApp</h2>
+                        <h2 class="text-center text-primary">Login To Ashsupport</h2>
                     </div>
                     <x-auth-session-status class="mb-4" :status="session('status')" />
 
