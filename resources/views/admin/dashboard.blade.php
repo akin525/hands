1018f5b472cbc239facb5e25ab2e9d94c0651023
HP <akinlabisamson15@gmail.com>
2022-09-26 22:28:21 +0100

    21/09/2022

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -133,14 +133,5 @@
     </div>
 
 
-    <script src="{{asset('vendors/scripts/core.js')}}"></script>
-    <script src="{{asset('vendors/scripts/script.min.js')}}"></script>
-    <script src="{{asset('vendors/scripts/process.js')}}"></script>
-    <script src="{{asset('vendors/scripts/layout-settings.js')}}"></script>
-    <script src="{{asset('src/plugins/jQuery-Knob-master/jquery.knob.min.js')}}"></script>
-    <script src="{{asset('src/plugins/highcharts-6.0.7/code/highcharts.js')}}"></script>
-    <script src="{{asset('src/plugins/highcharts-6.0.7/code/highcharts-more.js')}}"></script>
-    <script src="{{asset('src/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js')}}"></script>
-    <script src="{{asset('src/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-    <script src="{{asset('vendors/scripts/dashboard2.js')}}"></script>
+
 @endsection
